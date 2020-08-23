e6baa169f69ce60302d5b8183958a19a9274889f
Alex Varela <alexvar93@gmail.com>
2020-08-23 16:05:54 -0400

    updated writeToFile function

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const util = require("util");
+
 
 // array of questions for user
-const questions = [
+function promputUser() {
+inquirer.prompt([
     {
     type: "input",
     name: "title",
@@ -41,7 +45,7 @@ const questions = [
     {
     type: "input",
     name: "tests",
-    message: "TBD"
+    message: "How can you test your application?"
     },
     {
     type: "input",
@@ -52,22 +56,69 @@ const questions = [
     type: "input",
     name: "url",
     message: "Please enter your GitHub URL."
-    }
+    },
     {
     type: "input",
     name: "email",
     message: "Please enter your email."
     },
-];
+])};
 
-// function to write README file
+// function to write README file with retrieved user input
 function writeToFile(fileName, data) {
-}
+    // Using fs.writeFile to take three parameters and create the file.
+   fs.writeFile(fileName, data, function(err){
+       if(err) {
+        // If any error occurred when creating the file, show the error.
+        console.log(err);
+
+        // Otherwise, success!
+        console.log("Success! Your file was created.");
+       };
+   });
+};
 
 // function to initialize program
 function init() {
-
+    writeToFile();
 }
 
 // function call to initialize program
 init();
+
+
+
+// function promptUser() {
+//     return inquirer.prompt([
+//       {
+//         type: "input",
+//         name: "name",
+//         message: "What is your name?"
+//       },
+//       {
+//         type: "input",
+//         name: "location",
+//         message: "Where are you from?"
+//       },
+//       {
+//         type: "input",
+//         name: "hobby",
+//         message: "What is your favorite hobby?"
+//       },
+//       {
+//         type: "input",
+//         name: "food",
+//         message: "What is your favorite food?"
+//       },
+//       {
+//         type: "input",
+//         name: "github",
+//         message: "Enter your GitHub Username"
+//       },
+//       {
+//         type: "input",
+//         name: "linkedin",
+//         message: "Enter your LinkedIn URL."
+//       }
+//     ]);
+//   }
